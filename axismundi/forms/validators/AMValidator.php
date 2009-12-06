8805a878ca18931e33b08ab6a4a2b31331b63da6
aventurella <aventurella@gmail.com>
2009-12-06 12:16:55 -0800

    Initial commit

---
+++ axismundi/forms/validators/AMValidator.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *    AxisMundi
+ * 
+ *    Copyright (C) 2009 Adam Venturella
+ *
+ *    LICENSE:
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *    use this file except in compliance with the License.  You may obtain a copy
+ *    of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ *    PURPOSE. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    Author: Adam Venturella - aventurella@gmail.com
+ *
+ *    @package forms
+ *    @subpackage validators
+ *    @author Adam Venturella <aventurella@gmail.com>
+ *    @copyright Copyright (C) 2009 Adam Venturella
+ *    @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ *
+ **/
+abstract class AMValidator
+{
+	public $isRequired;
+	public $key;
+	public $message;
+	public $form;
+	public $isValid;
+	
+	protected $shouldRequire;
+	
+	protected function updateRequiredFlag($value)
+	{
+		if(strlen($value) > 0 && $this->shouldRequire)
+		{
+			$this->isRequired = true;
+		}
+		else
+		{
+			$this->isRequired = $this->shouldRequire ? false : true;
+		}
+	}
+	
+	abstract public function validate();
+}
+?>
